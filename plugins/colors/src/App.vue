b15ebfc2a893c160d1d20c2440686747292ba867
mekefly <mekefly@outlook.com>
2026-04-20 17:47:24 +0800

    加入了ZTollsApi

--- plugins/colors/src/App.vue
+++ plugins/colors/src/App.vue
@@ -1,4 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onPluginEnter, onPluginOut } from "@ztools-center/ztools-api-types";
+import { onMounted, ref } from "vue";
+
+let enterAction = ref<{
+  code: string;
+  type: string;
+  payload: any;
+  option: any;
+  from?: PluginEnterFrom;
+} | null>();
+let route = ref("");
+onMounted(() => {
+  onPluginEnter((action) => {
+    route.value = action.code;
+    enterAction.value = action;
+  });
+  onPluginOut(() => {
+    route.value = "";
+  });
+});
+</script>
 
 <template>
   <h1 class="text-3xl font-bold underline bg-red-200">You did it!</h1>
