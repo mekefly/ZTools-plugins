aa29adda18f561234a8ed48a47ba99cb92296e64
mekefly <mekefly@outlook.com>
2026-04-20 13:34:50 +0800

    配置tailwindcss

--- plugins/colors/src/App.vue
+++ plugins/colors/src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
+  <h1 class="text-3xl font-bold underline bg-red-200">You did it!</h1>
+  <p class="bg-red-900">
     Visit
     <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a>
     to read the documentation
--- plugins/colors/vite.config.ts
+++ plugins/colors/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite-plus";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   },
   fmt: {},
   lint: { options: { typeAware: true, typeCheck: true } },
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
